7ac87f30061a35678b93935455fc45dd5f7791a2
Muhammed Muhshif Karadan <65645312+muhshif@users.noreply.github.com>
2025-10-02 12:46:36 +0200

    Delete chapter08_capstone_project/src/visualize.py

--- chapter08_capstone_project/src/visualize.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt('../output/final_state.txt')
-plt.imshow(data, cmap='hot', interpolation='nearest')
-plt.colorbar(label='Temperature')
-plt.title('Final Heatmap')
-plt.savefig('heatmap.png')
-plt.show()
